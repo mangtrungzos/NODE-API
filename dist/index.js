7f8b3c8378a145013b136d8425d8ff9580214e6a
mangtrungzos <sangv906@gmail.com>
2025-01-08 10:24:59 +0700

    Build NODE API

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,14 @@
-import express from "express";
-import dotenv from "dotenv";
-
-dotenv.config({ path: __dirname + '/../.env' });
-const app = express();
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config({ path: __dirname + '/../.env' });
+const app = (0, express_1.default)();
 const port = process.env.PORT;
-app.use(express.json());
-
+app.use(express_1.default.json());
 let users = [
     {
         id: 1,
@@ -20,7 +23,6 @@ let users = [
         name: "Dev"
     }
 ];
-
 // CREATE
 app.post('/users', (req, res) => {
     const newUser = {
@@ -30,12 +32,10 @@ app.post('/users', (req, res) => {
     users.push(newUser);
     res.json(newUser);
 });
-
 // READ
 app.get('/users', (req, res) => {
     res.json(users);
 });
-
 // UPDATE
 app.put('/users', (req, res) => {
     const { id, name } = req.body;
@@ -47,8 +47,8 @@ app.put('/users', (req, res) => {
     });
     res.json(users);
 });
-
 // starts
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
+//# sourceMappingURL=index.js.map
